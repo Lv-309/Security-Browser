16a6edf3cae4c0f1dd66a06679612db006c10c87
Andrew <kovacz@ukr.net>
2018-05-21 12:45:25 +0300

    integrated Trace Utility

--- WebBrowser v0.9 (no stable)/WebBrowser2/TraceUtility/LogTrace.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "Log.h"
-#include "Severity.h"
-
-ISXLog::Log tlf_t(Severity::Trace);
---
+++ WebBrowser v0.9/WebBrowser2/TraceUtility/File.cpp
@@ -0,0 +1,18 @@
+#include "File.h"
+
+void File::OpenFile()
+{
+	this->m_ofile.open("info_file.log", std::ios_base::app);
+}
+
+void File::CloseFile()
+{
+	if (this->m_ofile.is_open())
+		this->m_ofile.close();
+}
+
+char* File::GetCurTime()
+{
+	std::time_t result = std::time(nullptr);
+	return std::asctime(std::localtime(&result));
+}
