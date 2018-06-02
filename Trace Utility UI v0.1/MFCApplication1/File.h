f6e3919b697b1885d5c925070b03336346e66f82
Denys Samozvon <densamozvon@gmail.com>
2018-06-02 23:17:32 +0300

    Delete unnecessary code.

--- Trace Utility UI v0.1/MFCApplication1/File.h
+++ Trace Utility UI v0.1/MFCApplication1/File.h
@@ -10,10 +10,7 @@ public:
 	void OpenFile(std::string path);
 	void CloseFile();
 	std::vector<std::string> ReadFromFile();
-	int GetLineCount();
 private:
 	std::ifstream ifile;
-	int line_counter = 0;
-
 	void Split(const std::string &s, char delim, std::vector<std::string> &elems);
 };
