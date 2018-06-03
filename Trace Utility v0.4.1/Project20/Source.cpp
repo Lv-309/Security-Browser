e112cd986a676ec796dafa78b9cd405d9e6019b1
samozvon <densamozvon@gmail.com>
2018-06-03 18:04:16 +0300

    Add version with edited mutex.

---
+++ Trace Utility v0.4.1/Project20/Source.cpp
@@ -0,0 +1,21 @@
+#include "Log.h"
+#include "LogDebug.h"
+#include "LogError.h"
+#include "LogWarning.h"
+#include "LogFatal.h"
+#include "LogInfo.h"
+#include "LogTrace.h"
+
+int main()
+{
+	tlf << AT << "Text1";
+	tlf_w << AT << "Text2"; 
+	tlf_f << AT << "Text3";
+	tlf_d << AT << "Text4";	
+	tlf_e << AT << "Text5";
+	tlf_i << AT << "Text6";
+	tlf_t << AT << "Text7";
+	
+	system("pause");
+	return 0;
+}
