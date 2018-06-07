7c6c7a1fa88aa8fcc8a4a0e07ac0b8df50ffbd2c
samozvon <densamozvon@gmail.com>
2018-06-07 17:08:26 +0300

    Revert "Update File.cpp"
    This reverts commit 13d818bdc3830e978db64820bcd1a78d6c0b6ac0.

--- SecureBrowser v0.9.2/SecureBrowser/TraceUtility/File.cpp
+++ SecureBrowser v0.9.2/SecureBrowser/TraceUtility/File.cpp
@@ -1,24 +1,18 @@
 #include "File.h"
 
-void ISXFile::File::OpenFile()
+void File::OpenFile()
 {
-	std::lock_guard<std::recursive_mutex> lock(mutex);
-
 	this->m_ofile.open("info_file.log", std::ios_base::app);
 }
 
-void ISXFile::File::CloseFile()
+void File::CloseFile()
 {
-	std::lock_guard<std::recursive_mutex> lock(mutex);
-
 	if (this->m_ofile.is_open())
 		this->m_ofile.close();
 }
 
-char* ISXFile::File::GetCurrentTime()
+char* File::GetCurTime()
 {
-	std::lock_guard<std::recursive_mutex> lock(mutex);
-
 	std::time_t result = std::time(nullptr);
 	return std::asctime(std::localtime(&result));
 }
