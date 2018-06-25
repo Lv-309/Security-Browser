4520a791496c1fcf2e17de9ee477622bbcdbfde2
Serhii <uolterbishep@gmail.com>
2018-06-25 14:07:58 +0300

    update Rest Client

---
+++ SecureBrowser v0.9.2/SecureBrowser/Rest Client/Base64.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+/**
+* base64_encode - Base64 encode
+* @src: Data to be encoded
+* @len: Length of the data to be encoded
+* @out_len: Pointer to output length variable, or %NULL if not used
+* Returns: Allocated buffer of out_len bytes of encoded data,
+* or empty string on failure
+*/
+
+std::string base64_encode(const unsigned char *src, size_t len);
---
+++ SecureBrowser v0.9.2/SecureBrowser/Rest Client/HttpClient.cpp
@@ -0,0 +1,86 @@
+#include <vector>
+#include <string>
+
+#include "HttpClient.h"
+#include "Base64.h" // for image decoding
+#include "LogInfo.h"
+
+namespace ISXHttpClient 
+{
+	HttpClient::HttpClient() : base_url(L"http://192.168.195.180"), client(base_url), 
+		user_name(L""), user_password(L"")
+	{
+	}
+
+	HttpClient::HttpClient(utility::string_t user_name, utility::string_t user_password) : user_name(user_name), user_password(user_password),
+		base_url(L"http://192.168.195.180"), client(base_url)
+	{
+	}
+
+	HttpClient::HttpClient(HttpClient&& rhs) : base_url(rhs.base_url), client(rhs.client)
+	{
+		// set rhs in deleted state
+	}
+
+	HttpClient::~HttpClient()
+	{
+	}
+
+	HttpClient& HttpClient::operator=(HttpClient&& rhs)
+	{
+		if (this != &rhs)
+		{
+			// free resources, nothing (no pointers)
+
+			base_url = rhs.base_url;
+			client = rhs.client;
+		}
+
+		// set rhs in deleted state, nothing (no pointers)
+
+		return *this;
+	}	
+
+	pplx::task<void> HttpClient::request_files_upload(char* buffer, size_t lenght, utility::string_t filename)
+	{		
+		std::string encoded_str = base64_encode(reinterpret_cast<const unsigned char*>(buffer), lenght);
+		std::wstring wstr_encoded(encoded_str.begin(), encoded_str.end());		
+
+		std::wstring body = L"/webservice/rest/server.php?&wstoken=859b2244c55636be03408c2b0e208b03&wsfunction=core_files_upload&contextid=0&component=user&filearea=draft&itemid=0&filepath=/&filename=";
+			
+		body.append(filename);
+		body.append(L"&filecontent=");
+		
+		body.append(wstr_encoded);
+		body.append(L"&contextlevel=user&instanceid=7");				
+		
+		return client.request(methods::POST, body).then([](http_response response)
+		{	
+			if (response.status_code() == status_codes::OK)
+			{
+				tlf_i << AT << "Server returned status code 200";
+			}			
+			
+			// output on a consol
+			std::wostringstream ss;
+			ss << L"Server returned returned status code " << response.status_code() << L'.' << std::endl;			
+			std::wcout << ss.str();
+		
+			// retrieve a response
+			size_t lenght = (size_t)response.headers().content_length();
+			istream bodyStream = response.body();
+
+			container_buffer<std::string> inStringBuffer;
+
+			return bodyStream.read(inStringBuffer, lenght).then([inStringBuffer](size_t bytesRead)
+			{
+				std::string &text = inStringBuffer.collection();
+				std::wstring wstr(text.begin(), text.end());
+
+				std::wcout << wstr << std::endl;				
+
+				tlf_i << AT << "Received responce";
+			});
+		});	
+	}
+}
