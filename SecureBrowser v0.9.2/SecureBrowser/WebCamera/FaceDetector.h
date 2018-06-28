0e81e3417873bbca2c15a8659fc09548091712f6
AIaska <38081956+AIaska@users.noreply.github.com>
2018-06-29 02:07:09 +0300

    Add files via upload

--- SecureBrowser v0.9.2/SecureBrowser/WebCamera/FaceDetector.h
+++ SecureBrowser v0.9.2/SecureBrowser/WebCamera/FaceDetector.h
@@ -4,27 +4,64 @@
 #include<vector>
 #include<chrono>
 
-#include<opencv2\objdetect.hpp>
+#include <opencv2\objdetect.hpp>
 
-#include"Camera.h"
+#include "../RestClient/Subject.h"
+#include "../RestClient/RestClient.h"
+#include"../TraceUtility/LogInfo.h"
+#include"../TraceUtility/LogWarning.h"
+#include"../TraceUtility/LogError.h"
+
+#include"Frame.h"
 
 namespace ISXFaceDetector
 {
-class FaceDetector : protected ISXCamera::Camera
-{
-public:
-	FaceDetector();
-	~FaceDetector() {};
+	class FaceDetector
+	{
+	public:
+		FaceDetector(int num_of_chosen_camera = 0); // add algorithm, path for saving
+		~FaceDetector() {};
 
-	void Detect();
-	void Check(const std::vector<cv::Rect>& faces);
-	void DrawRectAroundFaces(std::vector<cv::Rect> faces);
+		ISXFrame::Frame CaptureFrame();
 
-private:
-	cv::CascadeClassifier m_detector;
-	const std::string CASCADE_FILE = "haarcascade_frontalface_alt.xml";
-	ISXFrame::Frame m_frame;
-	/*std::chrono::seconds pause_time = std::chrono::seconds(3);
-	std::chrono::steady_clock::time_point last_time_check = std::chrono::high_resolution_clock::time_point(std::chrono::seconds(0));*/
-};
+		void RestartFaceDetector(int num_of_chosen_camera);
+		void StopFaceDetector();
+
+		//void TryFunc();
+
+		//TO DO
+		//add parameter to all detect functions
+		//save locally OR send to moddle
+
+		//algorithms for detecting suspicious behaviour and saving photos
+		//NOT FOR A SEPARATE THREAD
+		int SmartDetectAndSave(int test_duration_in_min = 40, int max_amount_of_photos = 50, int interval_in_sec_check_suspicious_behaviour = 5);
+		void SaveWithInterval(int interval_in_sec = 5);
+		void SaveConstantly();
+
+		// final algorithm for a separate thread (doesn't open an output window)
+		//FOR SEPARATE THREAD
+		int Run(int test_duration_in_min = 40, int max_amount_of_photos = 50, int interval_in_sec_check_suspicious_behaviour = 5);
+
+		// auxiliary functions
+		void DrawRectAroundFaces(std::vector<cv::Rect> faces);
+		bool PositionChanged(int x, int width, int y, int height, int delta = 15);
+
+
+	private:
+		// stop face detector if (stop == true)
+		std::atomic_bool stop = false;
+
+		cv::VideoCapture camera;
+		ISXFrame::Frame frame;
+		cv::CascadeClassifier detector;
+		const std::string CASCADE_FILE = "haarcascade_frontalface_alt.xml";
+
+		//face position
+		std::atomic_int x0 = 0;
+		std::atomic_int x1 = 0;
+		std::atomic_int y0 = 0;
+		std::atomic_int y1 = 0;
+	};
 }
+
--- SecureBrowser v0.9.2/SecureBrowser/WebCamera/Frame.h
+++ SecureBrowser v0.9.2/SecureBrowser/WebCamera/Frame.h
@@ -1,22 +1,66 @@
 #pragma once
 
-#include<opencv2\highgui.hpp>
-#include<opencv2\imgproc.hpp>
+#include <opencv2\highgui.hpp>
+#include <opencv2\imgproc.hpp>
+
+#include "../RestClient/Subject.h"
+#include "../RestClient/RestClient.h"
 
 namespace ISXFrame
 {
-class Frame
-{
-public:
-	Frame() {};
-	~Frame() {};
+	class Frame
+	{
+	public:
+		Frame() {};
+		~Frame() {};
 
-	void SaveFrameToFile(std::string saving_path_and_name) { cv::imwrite(saving_path_and_name, m_frame); }
-	void ShowFrame() { cv::imshow("Output", m_frame); }
+		void SaveFrameToFile(std::string saving_path_and_name) { cv::imwrite(saving_path_and_name, frame); }
 
-	cv::Mat& get_frame() { return m_frame; }
+		void SendToMoodle(std::wstring photo_name)
+		{
+			std::vector<unsigned char> buffer;
+			cv::imencode(".jpg", frame, buffer);
 
-private:
-	cv::Mat m_frame;
-};
+			Subject subj;
+			RestClient client(&subj);
+
+			//subj.setBufferData((char*)buffer.data(), buffer.size(), photo_name);
+		}
+
+		void ShowFrame()
+		{
+			if (frame.empty()) system("pause");
+			else cv::imshow("Output", frame);
+		}
+
+		bool IsFrameEmpty() //function used for unit testing only
+		{
+			if (frame.empty())
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+
+		cv::Mat& get_frame() { return  frame; }
+
+		Frame& operator = (const cv::Mat& new_frame)
+		{
+			this->frame = new_frame;
+			return *this;
+		}
+
+		Frame& operator = (const Frame& new_frame)
+		{
+			this->frame = new_frame.frame;
+			return *this;
+		}
+
+	private:
+		cv::Mat frame;
+	};
 }
+
