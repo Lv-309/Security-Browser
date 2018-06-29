14420bfb7ed14a991e2cd09b9a3f52c90ea602f6
AIaska <38081956+AIaska@users.noreply.github.com>
2018-06-29 17:52:47 +0300

    add

---
+++ SecureBrowser v0.9.2/SecureBrowser/RestClient/HttpClient.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <pplx/pplxtasks.h>
+#include <cpprest/filestream.h>
+#include <cpprest/base_uri.h>
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+using namespace concurrency;
+using namespace concurrency::streams;
+
+namespace ISXHttpClient 
+{
+	class HttpClient
+	{
+
+	public:
+		HttpClient();		
+		HttpClient(HttpClient&&);
+		~HttpClient();
+
+		HttpClient& operator=(HttpClient&&);
+		
+		pplx::task<void> request_files_upload(char* buffer, size_t lenght, std::wstring filename);	
+		bool openConfigFile(std::string);
+
+	private:
+		uri base_url;
+		http_client client;
+		std::wstring token;
+		std::wstring domen;
+		
+	};
+	
+}
+
